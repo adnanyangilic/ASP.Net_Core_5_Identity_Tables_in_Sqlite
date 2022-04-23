01e9f0f2c62c25df3807b08b5299fa235d68b99f
adnanyangilic <adnan.alistika@gmail.com>
2022-04-23 16:49:58 +0300

    Proje dosyası ekle.

---
+++ SqliteandIdentity23Nisan/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SqliteandIdentity23Nisan.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqliteandIdentity23Nisan.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees {get; set;}
+    }
+}
